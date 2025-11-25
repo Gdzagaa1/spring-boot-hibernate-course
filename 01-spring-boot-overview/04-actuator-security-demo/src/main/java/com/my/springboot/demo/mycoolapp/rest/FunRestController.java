c3f7b4404242e14bd297f4acb7a3cab4c8b0c53b
Giorgi Dzagania <giorgidzagania2299@gmail.com>
2025-11-25 17:50:13 +0400

    Actuator, securing endpoints

---
+++ 01-spring-boot-overview/04-actuator-security-demo/src/main/java/com/my/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,23 @@
+package com.my.springboot.demo.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World";
+    }
+
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run";
+    }
+
+    @GetMapping("/fortune")
+    public String getDatilyFortune() {
+        return "Lucky day";
+    }
+}
