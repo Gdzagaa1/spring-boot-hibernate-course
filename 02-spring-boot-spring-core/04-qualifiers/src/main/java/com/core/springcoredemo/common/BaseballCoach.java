42267f9ec270f8e51363e0c34eb8279d2436718f
Giorgi Dzagania <giorgidzagania2299@gmail.com>
2025-11-26 18:00:22 +0400

    Qualifiers annotation practice

---
+++ 02-spring-boot-spring-core/04-qualifiers/src/main/java/com/core/springcoredemo/common/BaseballCoach.java
@@ -0,0 +1,12 @@
+package com.core.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BaseballCoach implements Coach{
+
+    @Override
+    public String getDailyWorkout() {
+        return "30 minutes batting practice";
+    }
+}
