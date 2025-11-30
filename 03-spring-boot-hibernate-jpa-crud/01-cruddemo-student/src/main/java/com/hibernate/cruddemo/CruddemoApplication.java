155518282a262cc502a3538a74b0297749f42ece
Giorgi Dzagania <giorgidzagania2299@gmail.com>
2025-11-30 16:21:03 +0400

    Read objects with JPA. Add and Use findById method.

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/hibernate/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/hibernate/cruddemo/CruddemoApplication.java
@@ -19,8 +19,26 @@ public class CruddemoApplication {
 		return runner -> {
 			// createStudent(studentDAO);
 
-			createMultipleStudents(studentDAO);
+			// createMultipleStudents(studentDAO);
+			
+			readStudent(studentDAO);
 		};
+	}
+
+	private void readStudent(StudentDAO studentDAO) {
+		System.out.println("Creating new student object...");
+		Student tempStudent = new Student("Daffy", "Duck", "daffy@something.com");
+
+		System.out.println("Saving the student");
+		studentDAO.save(tempStudent);
+
+		int theId = tempStudent.getId();
+		System.out.println("Save student. Generated id: " + theId);
+
+		System.out.println("Retrieving student with id: " + theId);
+		Student myStudent = studentDAO.findById(theId);
+
+		System.out.println("Fount the student: " + myStudent);
 	}
 
 	private void createMultipleStudents(StudentDAO studentDAO) {
