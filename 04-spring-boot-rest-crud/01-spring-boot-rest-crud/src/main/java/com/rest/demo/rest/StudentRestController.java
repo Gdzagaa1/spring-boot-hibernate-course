e323cb9ca6549d84f781179761d7c104d013aa45
Giorgi Dzagania <giorgidzagania2299@gmail.com>
2025-12-10 16:45:06 +0400

    Add global exception handler/ControllerAdvice

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/rest/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/rest/demo/rest/StudentRestController.java
@@ -35,30 +35,6 @@ public class StudentRestController {
         return theStudents.get(studentId);
     }
 
-    // add an exception handler
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
-
-        StudentErrorResponse error = new StudentErrorResponse();
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-    }
-
-    // add another exception handler (any type exception)
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(Exception exc) {
-
-        StudentErrorResponse error = new StudentErrorResponse();
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
-    }
-
 
 
 }
